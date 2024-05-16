46ba354248cb7e2d569d03f7b17015cba369ecc6
Diya Kithani <diyakithani.2003@gmail.com>
2024-05-16 17:13:56 -0400

    SumThree done- needs checks at T

---
+++ SumThree.java
@@ -0,0 +1,14 @@
+public class SumThree {
+    public static void main(String[] args) {
+        if(args.length==3) {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = Integer.parseInt(args[2]);
+            int sum = a + b + c;
+            System.out.println(sum);
+        }
+        else{
+            System.out.println("need three arguments for this program to function");
+        }
+    }
+}
