db48e9a7c0165700c30ba4b6057dad9b398c4808
Diya Kithani <diyakithani.2003@gmail.com>
2024-05-16 17:07:21 -0400

    HiFour done- needs checks at T

---
+++ HiFour.java
@@ -0,0 +1,12 @@
+public class HiFour {
+    public static void main(String[] args) {
+        if (args.length == 4){
+            System.out.println(args[3]+" "+args[2]+" "+args[1]+" "+args[0]);
+
+        }
+        else{
+            System.out.println("need 4 args to operate this program");
+        }
+
+    }
+}
